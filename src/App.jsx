6ef1be2d971428463addaed4446e87ab786d126a
Rafi Hasan <rafihasan0075@gmail.com>
2023-02-12 12:00:00 +0600

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-function SquareBoard({value,onSquareBoardClick}) {
+function Square({value,onSquareClick}) {
  
   return (
     
-    <button onClick ={onSquareBoardClick} className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">{value}</button>
+    <button onClick ={onSquareClick} className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">{value}</button>
   );
 }
 
@@ -44,27 +44,27 @@ export default function Board() {
     <>
       <div>{status}</div>
       <div className="flex">
-        <SquareBoard value = {value[0]} onSquareBoardClick={()=> handleClick(0)}></SquareBoard>
-        <SquareBoard value = {value[1]} onSquareBoardClick={()=> handleClick(1)}></SquareBoard>
-        <SquareBoard value = {value[2]} onSquareBoardClick={()=> handleClick(2)}></SquareBoard>
+        <Square value = {value[0]} onSquareClick={()=> handleClick(0)}></Square>
+        <Square value = {value[1]} onSquareClick={()=> handleClick(1)}></Square>
+        <Square value = {value[2]} onSquareClick={()=> handleClick(2)}></Square>
       </div>
 
       <div className="flex">
-        <SquareBoard value = {value[3]} onSquareBoardClick={()=> handleClick(3)}></SquareBoard>
-        <SquareBoard value = {value[4]} onSquareBoardClick={()=> handleClick(4)}></SquareBoard>
-        <SquareBoard value = {value[5]} onSquareBoardClick={()=> handleClick(5)}></SquareBoard>
+        <Square value = {value[3]} onSquareClick={()=> handleClick(3)}></Square>
+        <Square value = {value[4]} onSquareClick={()=> handleClick(4)}></Square>
+        <Square value = {value[5]} onSquareClick={()=> handleClick(5)}></Square>
       </div>
 
       <div className="flex">
-        <SquareBoard value = {value[6]} onSquareBoardClick={()=> handleClick(6)}></SquareBoard>
-        <SquareBoard value = {value[7]} onSquareBoardClick={()=> handleClick(7)}></SquareBoard>
-        <SquareBoard value = {value[8]} onSquareBoardClick={()=> handleClick(8)}></SquareBoard>
+        <Square value = {value[6]} onSquareClick={()=> handleClick(6)}></Square>
+        <Square value = {value[7]} onSquareClick={()=> handleClick(7)}></Square>
+        <Square value = {value[8]} onSquareClick={()=> handleClick(8)}></Square>
       </div>
     </>
   );
 }
 
-function DisplayWinner(SquareBoards){
+function DisplayWinner(squares){
    let winningLogic = [
 
     [0,1,2],
@@ -81,8 +81,8 @@ function DisplayWinner(SquareBoards){
    for(let i = 0; i<winningLogic.length;i++){
     let [a,b,c] = winningLogic[i]
 
-    if(SquareBoards[a] && SquareBoards[a]===SquareBoards[b] && SquareBoards[a]===SquareBoards[c]){
-      return SquareBoards[a];
+    if(squares[a] && squares[a]===Squares[b] && Squares[a]===Squares[c]){
+      return Squares[a];
     }
    }
 
